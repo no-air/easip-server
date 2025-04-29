2a3ff9b60fa53b2008bf14e9d60c2c813aabc48f
Kwon770 <sckwon770@gmail.com>
2025-04-30 00:02:59 +0900

    init

---
+++ src/test/java/com/noair/easip/EasipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noair.easip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
