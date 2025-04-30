d381fce678d8f6c0b28b4a7661924c801dcdb832
jay <philipwq3963@gmail.com>
2025-04-30 21:06:07 +0900

    [noair-01] main merge한다.

---
+++ subs-app/src/main/java/com/noair/subsapp/EasipApplication.java
@@ -0,0 +1,13 @@
+package com.noair.subsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasipApplication.class, args);
+	}
+
+}
