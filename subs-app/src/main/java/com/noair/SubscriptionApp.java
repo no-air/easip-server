47550c09071ef6d21853ee6ca0b23e0700f5f120
jay <philipwq3963@gmail.com>
2025-04-30 17:42:48 +0900

    [noair-01] 멀티모듈 초기화한다.

---
+++ subs-app/src/main/java/com/noair/SubscriptionApp.java
@@ -0,0 +1,12 @@
+package com.noair;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class SubscriptionApp {
+    public static void main(String[] args) {
+        run(SubscriptionApp.class, args);
+    }
+}
