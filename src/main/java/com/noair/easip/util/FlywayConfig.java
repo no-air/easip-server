d3d728577aa05cde9825ab7ae96c8d8ef3b6a6ce
Kwon770 <sckwon770@gmail.com>
2025-05-25 23:21:45 +0900

    2

--- src/main/java/com/noair/easip/util/FlywayConfig.java
+++ src/main/java/com/noair/easip/util/FlywayConfig.java
@@ -1,5 +1,6 @@
-package com.noair.easip.config;
+package com.noair.easip.util;
 
+import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +9,9 @@ import org.springframework.context.annotation.Configuration;
 public class FlywayConfig {
 
     @Bean
-    public Flyway flywayBean() {
+    public Flyway flywayBean(DataSource dataSource) {
         Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
                 .baselineOnMigrate(true)
                 .load();
         flyway.repair();
